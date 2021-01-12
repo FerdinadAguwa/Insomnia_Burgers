7944cae41f7c057b3719d8faf85eab57c89a3b2c
Ferdinad Aguwa <ferd.aguwa@yahoo.com>
2021-01-12 16:33:35 -0600

    created sql files

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+USE burger_db;
+
+
+
+
+
+CREATE TABLE burger (
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR(30) NOT NULL,
+  devoured BOOLEAN NOT NULL,
+  PRIMARY KEY (id)
+);
