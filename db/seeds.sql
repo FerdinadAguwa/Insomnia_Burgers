6c0bc12987b9acfe0eeb0295ce3cfb3a3ee65e0e
Ferdinad Aguwa <ferd.aguwa@yahoo.com>
2021-01-13 21:52:47 -0600

    code refactory

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Double Cheese", true);
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Jalepeno Burger", true);
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("The Works", false);
